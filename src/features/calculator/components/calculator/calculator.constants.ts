afd1c00bb8cd1c4b1908c81887e88e8b9cb9cd13
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-02 23:20:01 -0600

    refactor: create the calculator constants file

---
+++ src/features/calculator/components/calculator/calculator.constants.ts
@@ -0,0 +1,25 @@
+export const symbols = [
+  [
+    { symbol: "7", type: "number" },
+    { symbol: "8", type: "number" },
+    { symbol: "9", type: "number" },
+    { symbol: "/", type: "operator" },
+  ],
+  [
+    { symbol: "4", type: "number" },
+    { symbol: "5", type: "number" },
+    { symbol: "6", type: "number" },
+    { symbol: "×", type: "operator" },
+  ],
+  [
+    { symbol: "1", type: "number" },
+    { symbol: "2", type: "number" },
+    { symbol: "3", type: "number" },
+    { symbol: "-", type: "operator" },
+  ],
+  [
+    { symbol: "0", type: "number" },
+    { symbol: "=", type: "equals" },
+    { symbol: "+", type: "operator" },
+  ],
+];
--- src/features/calculator/components/calculator/calculator.tsx
+++ src/features/calculator/components/calculator/calculator.tsx
@@ -1,32 +1,7 @@
 import CalculatorKey from "./calculator-key";
+import { symbols } from "./calculator.constants";
 import styles from "./calculator.module.css";
 import { useCalculator } from "./useCalculator.hook";
-
-const symbols = [
-  [
-    { symbol: "7", type: "number" },
-    { symbol: "8", type: "number" },
-    { symbol: "9", type: "number" },
-    { symbol: "/", type: "operator" },
-  ],
-  [
-    { symbol: "4", type: "number" },
-    { symbol: "5", type: "number" },
-    { symbol: "6", type: "number" },
-    { symbol: "×", type: "operator" },
-  ],
-  [
-    { symbol: "1", type: "number" },
-    { symbol: "2", type: "number" },
-    { symbol: "3", type: "number" },
-    { symbol: "-", type: "operator" },
-  ],
-  [
-    { symbol: "0", type: "number" },
-    { symbol: "=", type: "equals" },
-    { symbol: "+", type: "operator" },
-  ],
-];
 
 export default function Calculator() {
   const {
