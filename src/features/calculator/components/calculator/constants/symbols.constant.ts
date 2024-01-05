317d46d323a00d1164a3066a739ea4490c7c53df
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-05 14:44:12 -0600

    fix: update calculator symbols order

--- src/features/calculator/components/calculator/constants/symbols.constant.ts
+++ src/features/calculator/components/calculator/constants/symbols.constant.ts
@@ -3,29 +3,29 @@ export const SYMBOLS = [
     { symbol: "AC", type: "clear" },
     { symbol: "%", type: "operator" },
     { symbol: "+/-", type: "switchSign" },
+    { symbol: "/", type: "operator" },
   ],
   [
     { symbol: "7", type: "number" },
     { symbol: "8", type: "number" },
     { symbol: "9", type: "number" },
-    { symbol: "/", type: "operator" },
+    { symbol: "×", type: "operator" },
   ],
   [
     { symbol: "4", type: "number" },
     { symbol: "5", type: "number" },
     { symbol: "6", type: "number" },
-    { symbol: "×", type: "operator" },
+    { symbol: "-", type: "operator" },
   ],
   [
     { symbol: "1", type: "number" },
     { symbol: "2", type: "number" },
     { symbol: "3", type: "number" },
-    { symbol: "-", type: "operator" },
+    { symbol: "+", type: "operator" },
   ],
   [
     { symbol: "0", type: "number" },
     { symbol: ".", type: "decimal" },
     { symbol: "=", type: "equals" },
-    { symbol: "+", type: "operator" },
   ],
 ] as const;
