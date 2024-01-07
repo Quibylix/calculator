f016a93751ee5dc54971c483ff3b16aa86e9c519
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-07 16:28:29 -0600

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Calculator } from "@/features/calculator/components";
+import { Header } from "@/features/layout/components";
 import "./App.css";
 
 export default function App() {
   return (
     <main>
+      <Header />
       <Calculator />
     </main>
   );
